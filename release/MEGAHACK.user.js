815e213cef636521d07b34dc0f1930e90fa78f86
UUID1606 <108305740+UUID1606@users.noreply.github.com>
2022-12-31 19:42:54 +0000

    Update MEGAHACK.user.js

--- release/MEGAHACK.user.js
+++ release/MEGAHACK.user.js
@@ -1,1 +1,26 @@
-asdfsda
+// ==UserScript==
+// @name         Mega Hack JS
+// @version      1.00.0
+// @description  Tanki Online Cheat
+// @author       Zorro
+// @match        https://*.tankionline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+// @description  Tanki Online Cheat
+// @grant        GM_xmlhttpRequest
+
+// @downloadURL  https://github.com/UUID1606/MegaHack/raw/main/release/MEGAHACK.user.js
+// @updateURL    https://github.com/UUID1606/MegaHack/raw/main/release/MEGAHACK.meta.js
+
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+
+
+// ==/UserScript==
+
+GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://github.com/UUID1606/MegaHack/raw/main/release/MEGAHACK.min.js',
+    nocache: true,
+    onload: r => eval(r.responseText)
+})
