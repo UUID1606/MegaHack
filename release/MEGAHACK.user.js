7a38d699363b576de5c051749e8b2a572baffabd
UUID1606 <108305740+UUID1606@users.noreply.github.com>
2022-12-31 19:57:51 +0000

    Update MEGAHACK.user.js

--- release/MEGAHACK.user.js
+++ release/MEGAHACK.user.js
@@ -6,7 +6,6 @@
 // @match        https://*.tankionline.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
 // @description  Tanki Online Cheat
-// @grant        GM_xmlhttpRequest
 
 // @downloadURL  https://github.com/UUID1606/MegaHack/raw/main/release/MEGAHACK.user.js
 // @updateURL    https://github.com/UUID1606/MegaHack/raw/main/release/MEGAHACK.meta.js
